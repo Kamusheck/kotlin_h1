df7c93ddd46c7ee7464c46ea9054e7947f0dc2f1
Kamusheck <kismailova722@gmail.com>
2025-11-07 09:40:57 +0300

    fin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
 }
 
 dependencies {
@@ -50,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.foundation:foundation")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/kotlin_h1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_h1/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.kotlin_h1
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,36 +16,110 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlin_h1.ui.theme.Kotlin_h1Theme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Kotlin_h1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Kotlin_h1Theme {Myscreen()}
+
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Myscreen()
+{
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+    val rows = if (isPortrait) 3 else 4
+    var count by rememberSaveable { mutableStateOf(1) }
+
+    Box(modifier = Modifier
+        .fillMaxSize())
+    {
+        Column(modifier = Modifier
+            .fillMaxSize())
+        {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(rows),
+                modifier = Modifier.weight(1f),
+                contentPadding = PaddingValues(
+                    start = 8.dp,
+                    end = 8.dp,
+                    top = 8.dp,
+                    bottom = 80.dp
+                ) ,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ){
+                items(count){index -> Squares(number = index)
+                }
+            }
+        }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ){
+            Text (
+                stringResource(R.string.text)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Kotlin_h1Theme {
-        Greeting("Android")
+fun MyscreenPreview(){
+    Myscreen()
+}
+
+@Composable
+fun Squares(number: Int){
+    val color = if (number%2==0){
+        colorResource(R.color.chet)
+    }
+        else
+            colorResource(R.color.nechet)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .background(color),
+        contentAlignment = Alignment.Center
+
+    ){
+        Text(
+            text = number.toString(),
+            color = colorResource(R.color.number),
+            textAlign = TextAlign.Center
+        )
     }
 }
